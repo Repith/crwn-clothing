d18e63b4b951239a51c57ebaf91a3fe24e73f275
Repith <marcin.mcgajzler@gmail.com>
2023-02-15 13:43:02 +0100

    updating directory page + directory & category components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import "./categories.styles.scss";
 import React, { useState, useEffect } from "react";
+import Directory from "./components/directory/directory.component";
 
 const App = () => {
   const [categories, setCategories] = useState([]);
@@ -10,23 +10,7 @@ const App = () => {
       .then((categories) => setCategories(categories));
   }, []);
 
-  console.log(categories);
-  return (
-    <div className="categories-container">
-      {categories.map(({ title, id, imageUrl }) => (
-        <div className="category-container" key={id}>
-          <div
-            className="background-image"
-            style={{ backgroundImage: `url(${imageUrl})` }}
-          />
-          <div className="category-body-container">
-            <h2>{title}</h2>
-            <p>Shop now</p>
-          </div>
-        </div>
-      ))}
-    </div>
-  );
+  return <Directory categories={categories} />;
 };
 
 export default App;
