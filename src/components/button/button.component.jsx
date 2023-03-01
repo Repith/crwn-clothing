bee2a0891edb63dccfd3f8afa759098061aed187
Repith <marcin.mcgajzler@gmail.com>
2023-03-01 14:56:42 +0100

    +switched to styled component + styling buttons (change of workspace)

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -1,9 +1,21 @@
-import "./button.styles.scss";
+import {
+  BaseButton,
+  GoogleSignInButton,
+  InvertedButton,
+} from "./button.styles";
 
 const BUTTON_TYPE_CLASSES = {
+  base: "base",
   google: "google-sign-in",
   inverted: "inverted",
 };
+
+const getButton = (buttonType = BUTTON_TYPE_CLASSES.base) =>
+  ({
+    [BUTTON_TYPE_CLASSES.base]: BaseButton,
+    [BUTTON_TYPE_CLASSES.google]: GoogleSignInButton,
+    [BUTTON_TYPE_CLASSES.inverted]: InvertedButton,
+  }[buttonType]);
 
 const Button = ({ children, buttonType, ...otherProps }) => {
   return (
