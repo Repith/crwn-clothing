b8e092592366a477b9e68faccffca1885fbc9bc5
Repith <marcin.mcgajzler@gmail.com>
2023-03-07 17:14:59 +0100

    Migrating Category to Redux -categoriesContext

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,15 +1,17 @@
-import { useContext, useState, useEffect, Fragment } from "react";
+import { useState, useEffect, Fragment } from "react";
 import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 import ProductCard from "../../components/product-card/product-card.component";
 
-import { CategoriesContext } from "../../contexts/categories.context";
+import { selectCategoriesMap } from "../../store/categories/category.selector";
 
 import { Title, CategoryContainer } from "./category.styles";
 
 const Category = () => {
+  const categoriesMap = useSelector(selectCategoriesMap);
   const { category } = useParams();
-  const { categoriesMap } = useContext(CategoriesContext);
+
   const [products, setProducts] = useState(categoriesMap[category]);
 
   useEffect(() => {
@@ -20,7 +22,6 @@ const Category = () => {
   return (
     <Fragment>
       <Title>{category}</Title>
-
       <CategoryContainer>
         {products &&
           products.map((product) => (
