8a7188f9019c5292afc9b211690079174cef35c7
Repith <marcin.mcgajzler@gmail.com>
2023-03-10 13:57:10 +0100

    +ReduxPersist +ReduxDevTools usage +moving logger to separate file

--- src/store/store.js
+++ src/store/store.js
@@ -1,33 +1,42 @@
 import { compose, createStore, applyMiddleware } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 import logger from "redux-logger";
 
 import { rootReducer } from "./root-reducer";
 
-// const logger = createLogger();
+//Persist library helps
+//BL 'user' to avoid conflict with AuthStateListener
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["user"],
+};
 
-const middleWares = [process.env.NODE_ENV === "development" && logger].filter(
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+//.filter prevents from returning false as middleWares, and returns logger if dev mode is on
+const middleWares = [process.env.NODE_ENV !== "production" && logger].filter(
   Boolean
 );
 
-/* <<The example how does the logger flow works as a sequence of curried functions>>
-const ownLoggerMiddleware = (store) => (next) => (action) => {
-  if (!action.type) {
-    return next(action);
-  }
-
-  console.log("Type: ", action.type);
-  console.log("Payload: ", action.payload);
-  console.log("CurrentState: ", store.getState());
-
-  next(action);
-
-  console.log("Next state: ", store.getState());
-}; */
-
-//For middleware to work it's needed to compose it (spread ico other middlewares)
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+//If ReduxDevTools are available for use
+const composeEnhancer =
+  (process.env.NODE_ENV !== "production" &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+//For middleware to work it's needed to be composed (spread ico other middlewares)
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
 //createStore takes 3 arguments: 1) rootReducer (!)
 // 2) additional default state
 // 3) logger = shows state before a after action is dispatched, action itself, state  turn after
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedEnhancers
+);
+
+export const persistor = persistStore(store);
